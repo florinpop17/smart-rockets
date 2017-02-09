a37c809aa69466262b8f44902d3c3a00b3fd822a
Florin Pop <popflorin1705@yahoo.com>
2017-02-09 20:31:15 +0200

    package.json and basic express server

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, ()=> { console.log('Application running on port ',PORT)});
