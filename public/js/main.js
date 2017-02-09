026d06980f6315e13c83779bf6ef7354080a444c
Florin Pop <popflorin1705@yahoo.com>
2017-02-09 21:38:21 +0200

    when count reaches lifespan, count resets, new population(for now)

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,28 @@ var lifespan = 200; // User in DNA.js
 var count = 0; // Used also in rocket.js
 var lifeP;
 
+var target;
+
 function setup() {
     createCanvas(600, 600);
+
     lifeP = createP();
     population = new Population();
+
+    target = createVector(width/2, 50);
 }
 
 function draw() {
     background(51);
     lifeP.html('Count: '+count);
     count++;
+
+    if(count == lifespan){
+        population = new Population();
+        count = 0;
+    }
+
+    ellipse(target.x, target.y, 16, 16);
 
     population.updateRockets();
 }
