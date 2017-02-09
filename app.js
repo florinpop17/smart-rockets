e814bd664c842803d5878f7b3b91575d6c032594
Florin Pop <popflorin1705@yahoo.com>
2017-02-09 20:55:14 +0200

    res.render

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
 app.listen(PORT, ()=> { console.log('Application running on port ',PORT)});
